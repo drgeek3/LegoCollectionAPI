f61b58f6cd8a40eecfc871654d8d2757937b4b81
George Willson <drgeek3@gmail.com>
2025-05-24 21:48:08 -0500

    Corrected minimum number available value to allow 0 since that is possible.

--- Dtos/CreateOwnedDto.cs
+++ Dtos/CreateOwnedDto.cs
@@ -5,8 +5,8 @@ namespace LegoCollection.Dtos
     public record class CreateOwnedDto(
         [Required][StringLength(45)] string BrickId,
         [Required] int ColorId,
-        [Range(1,10000)] int NumAvailable,
-        [Range(0,10000)] int NumInUse,
+        [Range(0,100000)] int NumAvailable,
+        [Range(0,100000)] int NumInUse,
         [Required][StringLength(45)] string LocationId
     );
    
